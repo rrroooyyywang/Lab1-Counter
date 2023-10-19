225288381674a32ddfb20a78f018b356ec34b5e2
rrroooyyywang <104288292+rrroooyyywang@users.noreply.github.com>
2023-10-19 11:35:36 +0100

    Finish counter.sv

--- task1/counter.sv
+++ task1/counter.sv
@@ -1,5 +1,15 @@
-module counter(
-    
+module counter #(
+    parameter WIDTH = 8;
+)(
+    //interface signals
+    input logic clk,    //clock
+    input logic rst,    //reset
+    input logic en,     // counter enable
+    output logic [WIDTH - 1 : 0] count  //count output
 );
     
+    always_ff @(posedge clk)
+        if (rst) count <= {WIDTH{1'b0}};
+        else count + {(WIDTH - 1){1'b0},{en}}   // add 0,0,0,...,0,en
+        
 endmodule
